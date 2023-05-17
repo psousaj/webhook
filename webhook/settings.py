eb40887a885ba186649565b2e30159050f21381a
José <josepsousa2012@gmail.com>
2023-05-17 11:40:52 -0300

    tests

--- webhook/settings.py
+++ webhook/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_URL = 'staticfiles/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 
 # Default primary key field type
