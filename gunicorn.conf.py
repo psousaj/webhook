50052c04b43ee74dd6018e110cdab46f88336b3d
José Pinheiro <100180118+psousaj@users.noreply.github.com>
2023-06-15 14:00:40 -0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,9 +2,7 @@ import os
 import multiprocessing
 
 bind = "0.0.0.0:" + f"{os.environ.get('PORT', 8000)}"
-workers = multiprocessing.cpu_count() * 2 + 1
-threads = 4
+workers = multiprocessing.cpu_count()
 timeout = 120
 keepalive = 180
-max_requests = 1000
-worker_class = 'sync'
+
