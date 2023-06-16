b3faaea6e3d2f640dfffe433eac9a417781cff31
José Pinheiro <100180118+psousaj@users.noreply.github.com>
2023-06-16 20:57:05 -0300

    Update gunicorn.conf.py to dynoscale

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,6 @@
 import os
 import multiprocessing
+from dynoscale.hooks.gunicorn import pre_request  # noqa # pylint: disable=unused-import
 
 bind = "0.0.0.0:" + f"{os.environ.get('PORT', 8000)}"
 workers = multiprocessing.cpu_count()
