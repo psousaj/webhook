ab1279cd0d5b9a4ed5be6635dc9a6fd431663528
José Pinheiro <100180118+psousaj@users.noreply.github.com>
2023-07-19 17:44:18 -0300

    Update urls.py

--- webhook/urls.py
+++ webhook/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     path('webhook/contacts', include('contacts.urls')),
     path('webhook/control', include('control.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
